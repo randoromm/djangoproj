19a724f14b6bcf6a1464d5c531887cc852b12ae6
Rando R <rando_rommot@live.com>
2018-01-12 22:13:05 +0200

    Final polishings

--- personal/views.py
+++ personal/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'personal/home.html')
 
+
 def contact(request):
-    return render(request, 'personal/basic.html', {'content':['To contact me, just send me an email:', 'rando.rommot@itcollege.ee']})
+    return render(request, 'personal/basic.html', {'content': ['To contact me, just send me an email:',
+                                                               'rando.rommot@itcollege.ee']})
