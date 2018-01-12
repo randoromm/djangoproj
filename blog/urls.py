d5e9ea6199bdee83abce628c01b6b23d112fac39
Rando R <rando_rommot@live.com>
2018-01-12 09:32:14 +0200

    Blog app integrated to personal app. Individual blog post pages can be opened now.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import DetailView
+from blog.models import Post
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name='blog/post.html')),
 ]
