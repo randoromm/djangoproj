7e41cae3ae033c348e406341c766033c2aebdbf1
Rando R <rando_rommot@live.com>
2018-01-12 22:10:52 +0200

    Unittest added. Few polishings.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from .views import post_list
 
-# Create your tests here.
+
+class MyViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.resource = reverse('post_list')
+
+    def test_get(self):
+        request = self.factory.get(self.resource)
+        response = post_list(request)
+        self.assertEqual(response.status_code, 200)
+
