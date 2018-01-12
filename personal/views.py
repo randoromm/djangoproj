9bb0fdaea00c03cec1d5cf92a28afdd209282d28
Rando R <rando_rommot@live.com>
2018-01-12 08:47:57 +0200

    Personal/portfolio app pretty much finished. Added bootstrap and made a simple design for the website.

--- personal/views.py
+++ personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['To contact me, just send me an email:', 'rando.rommot@itcollege.ee']})
